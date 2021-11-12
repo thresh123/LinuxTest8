e6df228b6d7acf05258212f283b99fd9189bdc38
GY <3163855480@qq.com>
2021-11-12 16:28:09 +0800

    shell

---
+++ test4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read name animal1 animal2 animal3
+
+echo "welcom to our ${name}_zoo"
+echo "we have $animal1,$animal2,$animal3"
+
---
+++ testx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+read -a score
+for ((i=1;i < ${#score[*]};i++))
+do
+        for((j=0;j < ${#score[*]}-$i;j++))
+        do
+                if [ ${score[j]} -gt ${score[(($j+1))]} ]
+                then    temp=${score[j]}
+                        score[j]=${score[$(($j+1))]}
+                        score[$(($j+1))]=$temp
+                fi
+        done
+done
+echo ${score[*]}
+
